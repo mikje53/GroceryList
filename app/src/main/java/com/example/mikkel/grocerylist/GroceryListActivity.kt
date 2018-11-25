5fb2ad87655456c04e6bfe7083168a1918d83fe4
im.mikkel@gmail.com <im.mikkel@gmail.com>
2018-11-25 10:50:28 +0100

    Displaying static grocery list

--- app/src/main/java/com/example/mikkel/grocerylist/GroceryListActivity.kt
+++ app/src/main/java/com/example/mikkel/grocerylist/GroceryListActivity.kt
@@ -2,11 +2,17 @@ package com.example.mikkel.grocerylist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class GroceryListActivity : AppCompatActivity() {
+
+    val groceryList = listOf("Milk", "Eggs", "Butter", "Granola")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grocery_list)
+
+        val textView = findViewById<TextView>(R.id.grocery_list_text)
+        textView.text = groceryList.joinToString(separator = "\n")
     }
 }
