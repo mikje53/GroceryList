5f7360693941afe30bac5359aa30b0749beffd8c
im.mikkel@gmail.com <im.mikkel@gmail.com>
2018-11-25 10:10:32 +0100

    Created project

---
+++ app/src/main/java/com/example/mikkel/grocerylist/GroceryListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mikkel.grocerylist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GroceryListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grocery_list)
+    }
+}
