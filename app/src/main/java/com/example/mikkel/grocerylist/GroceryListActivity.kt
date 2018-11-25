964811205434d7cab0e8c99b6dcb9f0ebc240320
im.mikkel@gmail.com <im.mikkel@gmail.com>
2018-11-25 11:06:35 +0100

    Added functionality to add new items to the list.

--- app/src/main/java/com/example/mikkel/grocerylist/GroceryListActivity.kt
+++ app/src/main/java/com/example/mikkel/grocerylist/GroceryListActivity.kt
@@ -2,11 +2,13 @@ package com.example.mikkel.grocerylist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 
 class GroceryListActivity : AppCompatActivity() {
 
-    val groceryList = listOf("Milk", "Eggs", "Butter", "Granola")
+    val groceryList = mutableListOf("Milk", "Eggs", "Butter", "Granola")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +16,12 @@ class GroceryListActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.grocery_list_text)
         textView.text = groceryList.joinToString(separator = "\n")
+    }
+
+    fun addGroceryToList(view: View) {
+        val editText = findViewById<EditText>(R.id.add_item_edit_text)
+        val textView = findViewById<TextView>(R.id.grocery_list_text)
+        groceryList.add(editText.text.toString())
+        textView.text = groceryList.joinToString (separator = "\n")
     }
 }
