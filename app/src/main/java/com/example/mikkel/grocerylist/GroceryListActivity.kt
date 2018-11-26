d1de4d56207804c7b894a5abdcd0755da4251ac4
im.mikkel@gmail.com <im.mikkel@gmail.com>
2018-11-26 19:55:34 +0100

    Changed to use synthetic bindings. added a listview. Changed text in text field to be hint instead.

--- app/src/main/java/com/example/mikkel/grocerylist/GroceryListActivity.kt
+++ app/src/main/java/com/example/mikkel/grocerylist/GroceryListActivity.kt
@@ -3,25 +3,21 @@ package com.example.mikkel.grocerylist
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_grocery_list.*
 
 class GroceryListActivity : AppCompatActivity() {
 
-    val groceryList = mutableListOf("Milk", "Eggs", "Butter", "Granola")
+    private val groceryList = mutableListOf("Milk", "Eggs", "Butter", "Granola")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grocery_list)
 
-        val textView = findViewById<TextView>(R.id.grocery_list_text)
-        textView.text = groceryList.joinToString(separator = "\n")
+        groceryListTextView.text = groceryList.joinToString(separator = "\n")
     }
 
     fun addGroceryToList(view: View) {
-        val editText = findViewById<EditText>(R.id.add_item_edit_text)
-        val textView = findViewById<TextView>(R.id.grocery_list_text)
-        groceryList.add(editText.text.toString())
-        textView.text = groceryList.joinToString (separator = "\n")
+        groceryList.add(addItemEditText.text.toString())
+        groceryListTextView.text = groceryList.joinToString (separator = "\n")
     }
 }
